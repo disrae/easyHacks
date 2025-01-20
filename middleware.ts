e34654e96ea9c8d8f5a69c8d58c61e21d85c28c5
Daniel Israel <danny.israel@gmail.com>
2025-01-20 11:07:45 -0800

    Removed protected routes but left middleware function

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,6 @@
-import {
-  convexAuthNextjsMiddleware,
-  createRouteMatcher,
-  nextjsMiddlewareRedirect,
-} from "@convex-dev/auth/nextjs/server";
+import { convexAuthNextjsMiddleware } from "@convex-dev/auth/nextjs/server";
 
-const isSignInPage = createRouteMatcher(["/signin"]);
-const isProtectedRoute = createRouteMatcher(["/product(.*)"]);
-
-export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
-  const isAuthenticated = await convexAuth.isAuthenticated();
-  console.log(`Path: ${request.nextUrl.pathname}, isAuthenticated: ${isAuthenticated}`);
-  if (isSignInPage(request) && (await convexAuth.isAuthenticated())) {
-    console.log("Redirecting authenticated user from sign-in page to home");
-    return nextjsMiddlewareRedirect(request, "/");
-  }
-  if (isProtectedRoute(request) && !(await convexAuth.isAuthenticated())) {
-    console.log("Redirecting unauthenticated user to sign-in page");
-    return nextjsMiddlewareRedirect(request, "/");
-  }
-});
+export default convexAuthNextjsMiddleware();
 
 export const config = {
   // The following matcher runs middleware on all routes
