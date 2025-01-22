54fdd1749665833cdfdde7379d7ad021364b1473
Daniel Israel <danny.israel@gmail.com>
2025-01-22 03:39:36 -0800

    Added login on polls

--- app/polls/page.tsx
+++ app/polls/page.tsx
@@ -1,6 +1,8 @@
 import { PageHeader } from "@/components/PageHeader";
 import CreatePoll from "@/components/polls/CreatePoll";
 import { Polls } from "@/components/polls/Polls";
+import { SignIn } from "@/components/SignIn";
+import { Card, CardContent } from "@/components/ui/card";
 import { api } from "@/convex/_generated/api";
 import { convexAuthNextjsToken } from "@convex-dev/auth/nextjs/server";
 import { fetchQuery } from "convex/nextjs";
@@ -18,7 +20,15 @@ export default async function PollsPage() {
 
             <section className="container px-4 md:px-6 max-w-3xl mx-auto">
                 <div className="space-y-4 py-4">
-                    {user && <CreatePoll />}
+                    {user ? (
+                        <CreatePoll />
+                    ) : (
+                        <Card className="my-12 bg-black/50 border-gray-800 text-white hover:border-purple-500/50 transition-colors shadow-[inset_0_2px_4px_rgba(255,255,255,0.1)] hover:shadow-[inset_0_2px_8px_rgba(255,255,255,0.3)]">
+                            <CardContent className="p-6">
+                                <SignIn />
+                            </CardContent>
+                        </Card>
+                    )}
                     <Polls />
                 </div>
             </section>
